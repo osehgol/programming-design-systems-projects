fdeadd0d8f69e0ca0fca7dfd1547c605ddb53fa2
Osama Sehgol <osama.sehgol@gmail.com>
2016-09-08 15:43:19 -0400

    first assignment done

--- first-assignment/sketch.js
+++ first-assignment/sketch.js
@@ -1,3 +1,10 @@
+// You can only use black (0) and white (255). 
+// You are only allowed to use triangle(), rect() and ellipse() once each, 
+// and no other drawing functions are allowed 
+// (no beginShape or images). 
+// Bring to class a design of an ice cream cone. Yes, an ice cream cone.
+
+
 var r = new Rune({
   container: "#canvas",
   width: 800,
@@ -5,16 +12,21 @@ var r = new Rune({
   debug: true
 });
 
-r.rect(100, 100, 200, 200)
-  .fill(255, 0, 0)
-  .stroke(0)
+r.triangle(400, 250, 525, 600, 650, 250)
+  .fill(0, 0, 0)
+  .stroke(true)
 
-r.triangle(400, 50, 400, 400, 650, 50)
-  .fill(0, 255, 0)
-  .stroke(0)
+r.ellipse(525, 200, 250, 250)
+  .fill(255, 255, 255)
+  .stroke(true)
 
-r.ellipse(200, 500, 200, 300)
-  .fill(0, 0, 255)
-  .stroke(false)
+for(var i = 525; i < 650; i+=15){
+	for (var j = 0; j < 650; j+=15){
+	r.rect(i, j, 5, 5)
+  	 .fill(255)
+  	 .stroke(true)
+	}
+
+}  
 
 r.draw();
