6d1e2deed4c5fca8c3a854152065795495db26f5
Osama Sehgol <osama.sehgol@gmail.com>
2016-09-10 22:31:32 -0400

    assignment1 finished

--- first-assignment/sketch.js
+++ first-assignment/sketch.js
@@ -21,13 +21,24 @@ r.ellipse(525, 200, 250, 250)
   .fill(255, 255, 255)
   .stroke(true)
 
-for(var k = 525; k < 650; k+=15){
-	rectangle(k , 200);	
+var x = 100;
+for(var i = 0; i < 20; i++) {
+  r.rect(x, i * 20, 5, 5);
+  if(i < 10) {
+    x += 10;  
+  } else {
+    x -= 10;
+  }
+  
 }
 
-for(var k = 200; k < 335; k+=15){
-	rectangle(525,k);	
-}
+// for(var k = 525; k < 650; k+=15){
+// 	rectangle(k , 200);	
+// }
+
+// for(var k = 200; k < 335; k+=15){
+// 	rectangle(525,k);	
+// }
 
 // for(var i = 525; i < 650; i+=15){
 // 	for (var j = 200; j < 300; j+=15){
@@ -39,9 +50,9 @@ for(var k = 200; k < 335; k+=15){
 
 r.draw();
 
-function rectangle(i,j){
+// function rectangle(i,j){
 
-	r.rect(i,j,5,5)
-		.fill(255)
-		.stroke(true)
-}
+// 	r.rect(i,j,5,5)
+// 		.fill(255)
+// 		.stroke(true)
+// }
