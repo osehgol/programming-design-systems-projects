ffc81c641859954701b20bb803196d9ca7203adf
Osama Sehgol <osama.sehgol@gmail.com>
2016-09-27 16:12:51 -0400

    week 3 assignment

--- third-assignment/sketch.js
+++ third-assignment/sketch.js
@@ -1,29 +1,57 @@
 var r = new Rune({
   container: "#canvas",
-  width: 600,
-  height: 850
+  width: 1200,
+  height: 800,
+  frameRate: 24
 });
 
-var size = 50;
-var sides = [3, 4, 6, 40];
+// \/
+r.path(200, 150)
+  .lineTo(75,250)
+  .lineTo(125,0)
+  .curveTo(75,250,100,300,75.250)
+  .fill(255)
+  .closePath();
 
-for(var i = 0; i < sides.length; i++) {
+// \/
 
-  var shape = r.polygon(r.width/2, 125 + (i * 200));
+r.path(325,149)
+  .lineTo(75,250)
+  .lineTo(125,0)
+  .curveTo(75,250,100,300,75.250)
+  .closePath();
 
-  console.log(shape);
-  var angle = 360/sides[i];
-  console.log("spacing: "+angle);
+// droplet
+r.path(455, 400)
+ .curveTo()
 
-  for(var j = 0; j < sides[i]; j++) {
-    var x = Math.cos(r.radians(j * angle)) * size;
-    var y = Math.sin(r.radians(j * angle)) * size;
-    shape.lineTo(x, y);
+// playful circle
 
-console.log("x: "+x+" y: "+y);
+var circleSize = 100;
+var numPoints = 30;
+var angle = 360 / numPoints;
 
-  }
+// first make a polygon by using sin and cos
+var poly = r.polygon(200, 200)
+  .fill(false)
+  .stroke(0, 0, 0, 0.5);
 
+for(var i = 0; i < numPoints; i++) {
+  var x = Math.cos(Rune.radians(angle * i)) * circleSize;
+  var y = Math.sin(Rune.radians(angle * i)) * circleSize;
+  poly.lineTo(x, y);
 }
 
-r.draw();
+// r.on('draw', function(stage) {
+//   poly = poly.copy();
+//   for(var i = 0; i < poly.vars.vectors.length; i++){
+//     poly.vars.vectors[i].x += Rune.random(-2,2);
+//     poly.vars.vectors[i].y += Rune.random(-2,2);
+//   }
+// });
+
+r.on('mousemove', function(mouse) {
+  poly.move(mouse.x, mouse.y);
+});
+
+r.play();
